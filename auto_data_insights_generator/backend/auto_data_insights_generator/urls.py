df1f699b2f0616013d3ed4e654c2456538d2c413
Kartik-0108 <kartikdevadiga01@gmail.com>
2026-05-08 20:09:06 +0530

    update 3

--- auto_data_insights_generator/backend/auto_data_insights_generator/urls.py
+++ auto_data_insights_generator/backend/auto_data_insights_generator/urls.py
@@ -2,10 +2,11 @@
 URL configuration for auto_data_insights_generator project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -19,6 +20,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
 ]
 
+# Always serve media files (uploaded datasets & generated plots)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
